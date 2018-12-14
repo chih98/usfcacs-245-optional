82c3bbf28b4217ec61ed20c5376984538e9a1c8d
Marko Crnkovic <marko777.ny@gmail.com>
2018-12-14 02:23:44 -0800

    done

---
+++ GraphAdjMatrix.java
@@ -0,0 +1,29 @@
+/**
+ * GraphAdjMatrix.java
+ * @author Marko Crnkovic
+ */
+
+ public class GraphAdjMatrix implements Graph {
+    private int[][] adj;
+    
+    public static void main(String[] args) {
+
+        Graph graph = new GraphAdjMatrix(3);
+        graph.addEdge(0, 1, 3);
+        System.out.println(graph.getEdge(0,1));
+    }
+
+    public int getEdge(int vert1, int vert2){
+        int weight = adj[vert1][vert2];
+        return weight;   
+    }
+
+     public GraphAdjMatrix(int vertices){
+        adj = new int[vertices][vertices];
+    }
+
+    public void addEdge(int vert1, int vert2, int weight){
+        adj[vert1][vert2] = weight;
+    }
+
+ }
